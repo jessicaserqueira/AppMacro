bb8556bd4b386ed9cec3d9ff0b57d97b4eaa0b07
Jessica Serqueira <jessica.serqueira7@gmail.com>
2023-12-28 16:48:02 -0300

    first commit

---
+++ TesteMacroApp/TesteMacroApp/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  TesteMacroApp
+//
+//  Created by Jéssica on 28/12/23.
+//
+
+import SwiftUI
+import Testemacro
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+        .onAppear {
+            #stringify({
+                print(123)
+            }, {
+                print(85)
+            })
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TesteMacroApp/TesteMacroApp/TesteMacroAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TesteMacroAppApp.swift
+//  TesteMacroApp
+//
+//  Created by Jéssica on 28/12/23.
+//
+
+import SwiftUI
+
+@main
+struct TesteMacroAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
